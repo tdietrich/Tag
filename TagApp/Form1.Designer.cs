054f307068a220b6876cd01cc71242bd451550f6
Tomek <tomasz.dietrich@gmail.com>
2012-03-10 22:29:52 +0100

    Added Some English Txt for English Localization

--- TagApp/Form1.Designer.cs
+++ TagApp/Form1.Designer.cs
@@ -228,7 +228,7 @@
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(73, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(49, 17);
             this.toolStripStatusLabel1.Text = "No Jobs";
             // 
             // toolStripProgressBar1
--- TagApp/Form1.cs
+++ TagApp/Form1.cs
@@ -79,6 +79,8 @@ namespace TagApp
         }
         #endregion
 
+
+
     }
 }
 
