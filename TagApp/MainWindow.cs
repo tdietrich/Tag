b8ee8d780ccdb7c68e9e7242b29d0f33a8ef3ef1
Tomek <tomasz.dietrich@gmail.com>
2012-03-16 09:43:03 +0100

    blabalb

--- TagApp/MainWindow.cs
+++ TagApp/MainWindow.cs
@@ -16,6 +16,8 @@ namespace TagApp
      */
     public partial class MainWindow : Form
     {
+        List<TagLib.File> tablica = new List<TagLib.File>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,16 +34,18 @@ namespace TagApp
         {
             this.Close(); 
         }
+
         ////
         // Przycisk Dodaj Folder - prymitywne poczatkowe eventy dodane, wybieranie folderu, wypisywanie nazwy, zliczanie plikow mp3
         // i wypisywanie nizej w rich text boxie
         //
-        //!!!!!!!Przypisac DO wyszukiwania nowy wątek, zeby nie lagowalo!!!!!
-        //!!!!!!!Przypisac DO wyszukiwania progress bara z dolnego panelu
+        // TODO:Przypisac DO wyszukiwania nowy wątek, zeby nie lagowalo!
+        // TODO:Przypisac DO wyszukiwania progress bara z dolnego panelu
         //
+
         private void button1_Click(object sender, EventArgs e)
         {
-            int counter = 0;    //zlicza ilosc plikow - to mozna zalatwic chyba System.IO funkcja jakas
+            //int counter = 0;    //zlicza ilosc plikow - to mozna zalatwic chyba System.IO funkcja jakas
             string[] filePaths;
 
             DialogResult result = folderBrowserDialog1.ShowDialog();
@@ -52,30 +56,65 @@ namespace TagApp
                 isFilePathGiven.Text = folderBrowserDialog1.SelectedPath;
                 filesListingrichTextBox1.Text = "";
 
+                
                 //zdecyduj czy szukamy w subfolderach czy nie
-                if (ifSubfolders.Checked) filePaths = Directory.GetFiles(@folderBrowserDialog1.SelectedPath, "*.mp3", SearchOption.AllDirectories);
-                else filePaths = Directory.GetFiles(@folderBrowserDialog1.SelectedPath, "*.mp3");
+                if (ifSubfolders.Checked)
+                {
+                    filePaths = Directory.GetFiles(@folderBrowserDialog1.SelectedPath, "*.mp3", SearchOption.AllDirectories);
+                }
+                else
+                {
+                    filePaths = Directory.GetFiles(@folderBrowserDialog1.SelectedPath, "*.mp3");
+                }
 
                 //RICH TEXT BOX POMOCNY TYLKO BEDZIE PODCZAS DEV. POZNIEJ CHYBA DO WYKASOWANIA
-                if (filePaths.GetLength(0) != 0)
+                //if (filePaths.GetLength(0) != 0)
+                //{
+                //    foreach (string element in filePaths)
+                //    {
+                //        counter++;
+                //        // filesListingrichTextBox1.AppendText(element + "\n");
+                //    }
+                //}
+                //label1.Text = "Pliki mp3 w podanym folderze (" + counter.ToString() + ")";
+
+                if (filePaths.Length > 0)
                 {
-                    foreach (string element in filePaths)
+                    foreach (string str in filePaths)
                     {
+<<<<<<< HEAD
                         counter++;
                         filesListingrichTextBox1.AppendText(element + "\n");
+=======
+                        string[] info = new string[5];          // utworzenie tablicy stringów żeby dodawać wierszami
+                       
+                        tablica.Add(TagLib.File.Create(str));   // dodanie pliku do tablicy obiektów
+>>>>>>> 1971dc503d030ea802a6c88e9ce67950fecc72ff
 
+                        info[0] = tablica.First().Tag.FirstPerformer;
+                        info[1] = tablica.First().Tag.Album;
+                        info[2] = tablica.First().Tag.FirstGenre;
+                        info[3] = tablica.First().Tag.Title;
+                        info[4] = tablica.First().Tag.Year.ToString();
+
+                        mainGrid.Rows.Add(info);
                     }
-
                 }
-                label1.Text = "Pliki mp3 w podanym folderze (" + counter.ToString() + ")";
 
 
-            }
 
 
+<<<<<<< HEAD
            // TagLib.File plik = TagLib.File.Create("I:\\abc.mp3");
 
            // this.filesListingrichTextBox1.AppendText(plik.Tag.Album);
+=======
+            }
+
+
+            //TagLib.File plik = TagLib.File.Create("I:\\abc.mp3");
+            //this.filesListingrichTextBox1.AppendText(plik.Tag.Album);
+>>>>>>> 1971dc503d030ea802a6c88e9ce67950fecc72ff
 
 
         }
